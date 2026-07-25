3388d95d9c0232c099822b124f6ca8900d93e0bb
rtxyd <65117312+rtxyd@users.noreply.github.com>
2026-07-26 00:46:43 +0800

    Fix HighPriest crash of dungeons_and_combat

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+# 1.0.1
+- Fix HighPriest crash of dungeons_and_combat
+
 # 1.0.0 
 - Initial Release
---
+++ src/main/java/net/kayn/soulrend_tweaks/mixin/HighPriestFix.java
@@ -0,0 +1,22 @@
+package net.kayn.soulrend_tweaks.mixin;
+
+import net.mcreator.dungeonsandcombat.procedures.HighPriestOnEntityTickUpdateProcedure;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.level.LevelAccessor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(HighPriestOnEntityTickUpdateProcedure.class)
+public class HighPriestFix {
+    @Inject(method = "execute", at = @At("HEAD"), cancellable = true)
+    private static void a(LevelAccessor world, double x, double y, double z, Entity entity, CallbackInfo ci) {
+        if (entity instanceof Mob mob) {
+            if (mob.getTarget() == null) {
+                ci.cancel();
+            }
+        }
+    }
+}
